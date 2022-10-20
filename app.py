220b8fbca7a812c79773bc3e0ccc10444b474caf
Adithya Subramani R <77055902+adithya-subramani@users.noreply.github.com>
2022-10-20 20:36:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ model = tf.keras.models.load_model('Model.h5')
 def predict(inp):
   prediction = model.predict(np.array([tf.keras.preprocessing.image.img_to_array(inp)]) )
   return (1-prediction)*100
-gr.Interface(fn=predict,inputs=gr.Image(shape=(224, 224)),outputs=gr.Number()).launch()
+gr.Interface(fn=predict,inputs=gr.Image(shape=(224, 224)),outputs=gr.Number(),server_name="0.0.0.0").launch()
