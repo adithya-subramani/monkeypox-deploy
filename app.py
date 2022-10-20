ee9ffe5026976aa759890fe0db1f4341a6ace292
Adithya Subramani R <77055902+adithya-subramani@users.noreply.github.com>
2022-10-20 20:18:12 +0530

    Files for deploying

---
+++ app.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import gradio as gr
+import numpy as np
+model = tf.keras.models.load_model('Model.h5')
+def predict(inp):
+  prediction = model.predict(np.array([tf.keras.preprocessing.image.img_to_array(inp)]) )
+  return (1-prediction)*100
+gr.Interface(fn=predict,inputs=gr.Image(shape=(224, 224)),outputs=gr.Number()).launch()
